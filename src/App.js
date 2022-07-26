3291a2c05f8b09226f407e4d65da710fa9f66f94
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-26 16:35:44 +0600

    nav-bar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Main from './Components/Main';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <React.Fragment>
+      <Main />
+    </React.Fragment>
   );
 }
 

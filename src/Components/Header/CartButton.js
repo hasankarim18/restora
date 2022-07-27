358e9b06f2183a57a2f1e3cd79c2e87d31373658
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-27 09:41:38 +0600

    cart item on cartbutton

--- src/Components/Header/CartButton.js
+++ src/Components/Header/CartButton.js
@@ -4,10 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { Context } from '../../Context/Context'
 
+
 const CartButton = (props) => {
 
     const ctx = useContext(Context)
     // console.log(ctx)
+
+    const numberOfCartItem = ctx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount
+    }, 0)
+
     return (
         <div onClick={ctx.onShowCart} className={`${classes.CartButton}`} >
             <div className="d-flex justify-content-center align-items-center" >
@@ -20,7 +26,7 @@ const CartButton = (props) => {
                 </span>
                 <span>
                     <span className={classes.cartNumber} >
-                        0
+                        {numberOfCartItem}
                     </span>
                 </span>
             </div>

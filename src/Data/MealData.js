ae0c3c059b1a25f930b946cdfdddbeeebd3bb4cf
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-27 20:07:52 +0600

    image added

---
+++ src/Data/MealData.js
@@ -0,0 +1,30 @@
+export const MealData = [
+    {
+        id: 'm1',
+        name: 'Salmon',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+        type: 'Fish'
+    },
+    {
+        id: 'm2',
+        name: 'Beef Stake',
+        description: 'A organic beaf!',
+        price: 16.5,
+        type: 'meat'
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+        type: 'fast food'
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+        type: 'Vegan'
+    },
+];
